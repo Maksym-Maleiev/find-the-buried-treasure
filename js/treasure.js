23db2c2aab8d9e904d7759d91dad4d0337e07f25
Maksym-Maleiev <maximmaleev93@gmail.com>
2023-12-26 09:55:28 +0200

    Update treasure.js

--- js/treasure.js
+++ js/treasure.js
@@ -15,3 +15,9 @@ var clicks = 0;
 $("#map").click(function (event) {
   clicks++;
 });
+
+var getDistance = function (event, target) {
+  var diffX = event.offsetX - target.x;
+  var diffY = event.offsetY - target.y;
+  return Math.sqrt((diffX * diffX) + (diffY * diffY));
+};
