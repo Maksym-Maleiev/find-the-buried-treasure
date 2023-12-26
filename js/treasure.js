6fadefe4237dd8c23657b8708346d5a594d5606e
Maksym-Maleiev <maximmaleev93@gmail.com>
2023-12-26 10:23:24 +0200

    Update treasure.js

--- js/treasure.js
+++ js/treasure.js
@@ -1,27 +1,16 @@
+// Отримуємо випадкове число від 0 до size
 var getRandomNumber = function (size) {
   return Math.floor(Math.random() * size);
 };
 
-var width = 400;
-var height = 400;
-
-var target = {
-  x: getRandomNumber(width),
-  y: getRandomNumber(height)
-};
-
-var clicks = 0;
-
-$("#map").click(function (event) {
-  clicks++;
-});
-
+// Вираховуємо відстань між подією та ціллю
 var getDistance = function (event, target) {
   var diffX = event.offsetX - target.x;
   var diffY = event.offsetY - target.y;
   return Math.sqrt((diffX * diffX) + (diffY * diffY));
 };
 
+// Отримуємо рядок, що показує відстань
 var getDistanceHint = function (distance) {
   if (distance < 10) {
     return "Boiling hot!";
@@ -40,10 +29,33 @@ var getDistanceHint = function (distance) {
   }
 };
 
-var distance = getDistance(event, targrt);
+// Налаштовуємо наші змінні параметри
+var width = 400;
+var height = 400;
+var clicks = 0;
+
+// Створюємо випадкову локацію target
+var target = {
+  x: getRandomNumber(width),
+  y: getRandomNumber(height)
+};
+
+// Додаємо маніпулятор кліків до елемента img
+$("#map").click(function (event) {
+  clicks++;
+});
+
+// Отримуємо відстань між event і target
+var distance = getDistance(event, target);
+
+// Конвертуємо відстань у підсказку
 var distanceHint = getDistanceHint(distance);
+
+// Оновлюємо #distance-елемент новою підсказкою
 $("#distance").text(distanceHint);
 
+// Якщо клік було зроблено достатньо близько,
+// кажемо гравцеві, що він переміг
 if (distance < 8) {
   alert("Found the treasure in " + clicks + " clicks!");
 }
